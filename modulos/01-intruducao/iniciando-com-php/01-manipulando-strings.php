77de3db7611a9fde29e2e21a6657f660a874f757
Joabe Kachorroski <kernelkill@outlook.com>
2017-02-01 12:19:52 -0300

    Manipulando string, adicionando mais informaçoes.

--- modulos/01-intruducao/iniciando-com-php/01-manipulando-strings.php
+++ modulos/01-intruducao/iniciando-com-php/01-manipulando-strings.php
@@ -19,4 +19,25 @@ echo "<br>";
 for ($i = 1; $i <= 10; $i++):
     echo str_pad($i, 2, 0, STR_PAD_LEFT)."<br>";
 endfor;
+
+echo "<hr>";
+
+$escreva = "Desculpa, ";
+echo str_repeat($escreva, 100);
+
+echo "<hr>";
+
+$NovoNome = "Joaberson Guimaraes Kachorroski<br>";
+
+echo substr($NovoNome, 0, 6);
+echo "<br>";
+
+echo substr($NovoNome, 0, strpos($NovoNome, ' '));
+echo substr($NovoNome, strpos($NovoNome, ' '));
+
+echo "<br>";
+
+echo substr($NovoNome, 0, strpos($NovoNome, ' '));
+echo substr($NovoNome, strripos($NovoNome, ' '));
+
 ?>
