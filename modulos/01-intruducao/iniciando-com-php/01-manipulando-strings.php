468758147cd47cf949ded97d01d491e003031d6d
Joabe Kachorroski <kernelkill@outlook.com>
2017-02-01 11:45:37 -0300

    Manipulando string

--- modulos/01-intruducao/iniciando-com-php/01-manipulando-strings.php
+++ modulos/01-intruducao/iniciando-com-php/01-manipulando-strings.php
@@ -1,4 +1,22 @@
-
 <?php
+header('Content-Type: text/html; charset=utf-8');
 
+$nome = "joabe kachorroski <br>";
+
+echo strtoupper($nome) ;
+echo strtolower($nome);
+echo ucwords($nome);
+
+echo "<hr>";
+
+$img = "NOME DA IMAGEM.jpg";
+
+$imagem = strtolower(str_replace(' ', '-', $img));
+
+echo $imagem;
+echo "<br>";
+
+for ($i = 1; $i <= 10; $i++):
+    echo str_pad($i, 2, 0, STR_PAD_LEFT)."<br>";
+endfor;
 ?>
