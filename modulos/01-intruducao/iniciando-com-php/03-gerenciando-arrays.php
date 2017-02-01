bb89db978895d058df00dd18806200eb5b7acde1
Joabe Kachorroski <kernelkill@outlook.com>
2017-02-01 17:47:13 -0300

    Gerenciando Arrays, arrays e etc.

--- modulos/01-intruducao/iniciando-com-php/03-gerenciando-arrays.php
+++ modulos/01-intruducao/iniciando-com-php/03-gerenciando-arrays.php
@@ -1,8 +1,22 @@
 <?php
+header('Content-Typ: text/html; charset=utf-8');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$Pessoa = [];
+$Pessoa['nome'] = "Joabe Guimaraes Q. Kachorroski <script><?=";
+$Pessoa = array_pad($Pessoa, 5, NULL); //Crio posiçoes nulas dentro do array
 
+$Pessoa = array_filter($Pessoa);
+$Pessoa = array_map('strip_tags', $Pessoa);
+$Pessoa = array_map('trim', $Pessoa);
+
+array_push($Pessoa, 29);
+array_pop($Pessoa);
+array_unshift($Pessoa, 29);
+array_shift($Pessoa);
+
+
+$Pessoa['idade'] = 25;
+$Pessoa['job'] = 'Webmaster';
+
+
+var_dump($Pessoa);
