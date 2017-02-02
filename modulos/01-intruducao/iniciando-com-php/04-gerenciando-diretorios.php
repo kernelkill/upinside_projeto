57e1b96be7cfc15fa914daacc99b519bfa6fa9cc
Kachorroski <kernelkill@outlook.com>
2017-02-02 12:43:58 -0300

    Comitando alguns programas

--- modulos/01-intruducao/iniciando-com-php/04-gerenciando-diretorios.php
+++ modulos/01-intruducao/iniciando-com-php/04-gerenciando-diretorios.php
@@ -1,4 +1,23 @@
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
 
+function checkDir($Dir){
+    if (file_exists($Dir) && is_dir($Dir)):
+        return true;
+    else:
+        return false;
+    endif;
+}
+
+
+$getDir = getcwd();//Pega nome do diretorio atual
+$setDir = "{$getDir}/04";
+echo "{$getDir}<hr>";
+
+if (!checkDir($setDir)):
+    mkdir($setDir, 0777);
+endif;
+
+echo str_repeat("<br>", 40);
 ?>
