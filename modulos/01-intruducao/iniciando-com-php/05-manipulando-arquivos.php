07fe00213d6ec21fb631f4b51948f8b1a3592d46
Kachorroski <kernelkill@outlook.com>
2017-02-02 22:37:52 -0300

    Manipulando arquivo em diretorio

--- modulos/01-intruducao/iniciando-com-php/05-manipulando-arquivos.php
+++ modulos/01-intruducao/iniciando-com-php/05-manipulando-arquivos.php
@@ -1,4 +1,18 @@
 
 <?php
+header('Content-Type: text/html; charset=utf-8');
+
+$baseDir = getcwd() . "/05"; //Abre pasta atual
+echo $baseDir; //mostra onde estamos
+echo "<hr>";
+
+//r = ler
+//w = escrever
+//a = anexar
+
+$File = fopen("{$baseDir}/05.txt", 'w');//cria um novo arquivo dentro da pasta atual
+$Txt = "Joabe Kachorros\r\nhttps://facebook.com/JKACTO";//cria o texto
+fwrite($File, $Txt);//escreve no arquivo oq foi inserido ali em cima
+fclose($File);//fecha arquivo
 
 ?>
