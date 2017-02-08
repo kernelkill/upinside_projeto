78e97459e0e8edd422463925af4ec9a003b5bb74
Kachorroski <kernelkill@outlook.com>
2017-02-07 23:36:18 -0300

    Segue aula.

---
+++ modulos/01-intruducao/modulo-classe/class/AtributosMetodos.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class AtributosMetodos {
+    
+    //Atributos
+    var $Nome;
+    var $Idade;
+    var $Profissao;
+    
+    function setUsuario($Nome, $Idade, $Profissao){
+        echo $Nome;
+    }
+    
+    function getUsuario(){
+        
+    }
+}
