918fd694a6f1ce1239cfb3d2fc5c3aba5f061f63
Kachorroski <kernelkill@outlook.com>
2017-02-07 23:02:46 -0300

    Criando funcoes.

--- modulos/01-intruducao/modulo-classe/01-classes-e-objetos.php
+++ modulos/01-intruducao/modulo-classe/01-classes-e-objetos.php
@@ -6,7 +6,15 @@
     </head>
     <body>
         <?php
-        // put your code here
+            require('./class/ClassesObjeto.class.php');
+            
+            $teste = new ClassesObjeto();
+            $teste->getClass('De introduçao', 'mostrar uma classe.');
+            
+            $teste->Classe = 'Classe 02';
+            $teste->Funcao = 'Recebe uma funcao';
+            
+            $teste->verClass();
         ?>
     </body>
 </html>
