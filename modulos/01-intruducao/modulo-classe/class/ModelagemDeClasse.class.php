e52ed92d93b43f940fd015d80e1d9a813b8fd04a
Kachorroski <kernelkill@outlook.com>
2017-02-08 23:11:04 -0300

    Modelagem de Classe continua

--- modulos/01-intruducao/modulo-classe/class/ModelagemDeClasse.class.php
+++ modulos/01-intruducao/modulo-classe/class/ModelagemDeClasse.class.php
@@ -6,5 +6,41 @@
  * @copyright (c) year, Joabe G. Q. Kachorroski
  */
 class ModelagemDeClasse {
-//put your code here
+    
+    public $Nome;
+    public $Idade;
+    public $Profissao;
+    public $ContaSalario;
+    
+    function __construct($Nome, $Idade, $Profissao, $ContaSalario) {
+        $this->Nome = $Nome;
+        $this->Idade = $Idade;
+        $this->Profissao = $Profissao;
+        $this->ContaSalario = $ContaSalario;
+    }
+    
+    public function setNome($Nome) {
+        $this->Nome = $Nome;
+    }
+
+    public function setIdade($Idade) {
+        $this->Idade = $Idade;
+    }
+
+    public function setProfissao($Profissao) {
+        $this->Profissao = $Profissao;
+    }
+
+    public function setContaSalario($ContaSalario) {
+        $this->ContaSalario = $ContaSalario;
+    }
+    
+    public function ToReal($Valor){
+        return number_format($Valor,'2', '.', ',');
+        
+    }
+    
+    public function DarEcho($Mensagem){
+        echo "<p>{$Mensagem}</p>";
+    }
 }
