6e71c5d906908de67f608efa7b93e71a7f38e109
Kachorroski <kernelkill@outlook.com>
2017-02-15 21:28:27 -0300

    Comecando a trabalhar com heranças.

---
+++ modulos/03-heranca/01-conceitos-de-heranca.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Heranca PHP</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kachorroski
+ * Date: 15/02/17
+ * Time: 21:22
+ */
+?>
+
+</body>
+</html>
+
