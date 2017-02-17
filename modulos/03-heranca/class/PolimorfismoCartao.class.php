534e84e759aee739adb5e8f4d6e8d758cdfb58d7
Kachorroski <kernelkill@outlook.com>
2017-02-16 23:35:16 -0300

    Ainda trabalhando com polimorfismo.

--- modulos/03-heranca/class/PolimorfismoCartao.class.php
+++ modulos/03-heranca/class/PolimorfismoCartao.class.php
@@ -6,7 +6,44 @@
  * Date: 15/02/17
  * Time: 22:50
  */
-class PolimorfismoCartao
+class PolimorfismoCartao extends Polimorfismo
 {
+    public $Juros;
+    public $Encargos;
+    public $Parcelas;
+    public $NumParcelas;
+
+    function __construct($Produto, $Valor)
+    {
+        parent::__construct($Produto, $Valor);
+        $this->Juros = 1.17;
+        $this->Metodo = 'Cartao de Cretido';
+    }
+
+    /**
+     * @param float $Juros
+     */
+    public function setJuros($Juros)
+    {
+        $this->Juros = $Juros;
+    }
+
+    /**
+     * @param mixed $Encargos
+     */
+    public function setEncargos($Encargos)
+    {
+        $this->Encargos = $Encargos;
+    }
+
+    /**
+     * @param mixed $NumParcelas
+     */
+    public function setNumParcelas($NumParcelas)
+    {
+        $this->NumParcelas = $NumParcelas;
+    }
+
+
 
 }
