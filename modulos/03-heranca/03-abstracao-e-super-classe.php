6121409eb476bcd5857bc7ba0c6d5dfdc0d1e10c
Kachorroski <kernelkill@outlook.com>
2017-02-20 21:57:42 -0400

    Iniciando os trabalho de abastraçao.

--- modulos/03-heranca/03-abstracao-e-super-classe.php
+++ modulos/03-heranca/03-abstracao-e-super-classe.php
@@ -1,7 +1,28 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Abstração</title>
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
  * User: kachorroski
  * Date: 17/02/17
  * Time: 00:07
- */
+ */ 
+    require_once ('./inc/config.inc.php');
+    
+    $conta = new Abstracao('Joabe Kachorroski', 500);
+    $conta->Depositar(1000);
+
+    var_dump($conta);
+
+?>
+
+</body>
+</html>
---
+++ modulos/03-heranca/class/Abstracao.class.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kachorroski
+ * Date: 17/02/17
+ * Time: 00:07
+ */
+class Abstracao
+{
+    public $Cliente;
+    public $Conta;
+    public $Saldo;
+
+    /**
+     * Abstracao constructor.
+     * @param $Cliente
+     * @param $Saldo
+     */
+    public function __construct($Cliente, $Saldo)
+    {
+        $this->Cliente = $Cliente;
+        $this->Saldo = $Saldo;
+    }
+
+    public function Depositar($Valor)
+    {
+        $this->Saldo += (float) $Valor;
+        echo "<spam style='color: green'><b> {$this->Conta}:</b> Deposito de {$this->Real($Valor)} efetuado com sucesso!</spam><br>";
+    }
+
+    public function Sacar($Valor)
+    {
+        $this->Saldo -= (float) $Valor;
+        echo "<spam style='color: red'><b> {$this->Conta}:</b> Deposito de {$this->Real($Valor)} efetuado com sucesso!</spam><br>";
+    }
+
+    /** @param Abstracao $Destino */
+    public function Transferir($Valor, $Destino)
+    {
+        if ($this === $Destino):
+            echo "Voce nao pode tranferir";
+        else:
+            echo "<hr>";
+            $this->Sacar($Valor);
+            $Destino->Depositar($Valor);
+            echo "<spam style='color:blue'><b> {$this->Conta}:</b> Tranferencia de {$this->Real($Valor)} efetuada com sucesso de {$this->Cliente} para {$Destino->Cliente}.</spam><br>";
+        endif;
+        
+    }
+
+    public function Extrato()
+    {
+        echo "<hr>Ola {$this->Cliente}. Seu saldo em {$this->Conta} e de {$this->Real($this->Saldo)}<hr>";
+
+    }
+
+    public function Real($Valor)
+    {
+        return "R$ " . number_format($Valor, '2', '.', ',');
+    }
+
+
+}
