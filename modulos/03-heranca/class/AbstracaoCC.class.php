20ce529ef3346d27780816d0d0bbcbb14754dfa4
Kachorroski <kernelkill@outlook.com>
2017-02-21 22:13:14 -0400

    Herdando classes abastratas.

--- modulos/03-heranca/class/AbstracaoCC.class.php
+++ modulos/03-heranca/class/AbstracaoCC.class.php
@@ -8,6 +8,14 @@
  */
 class AbstracaoCC extends Abstracao
 {
+    public $Limite;
+
+    function __construct($Cliente, $Saldo, $Limite)
+    {
+        parent::__construct($Cliente, $Saldo);
+        $this->Conta = 'Conta Corrente';
+        $this->Limite = (float) $Limite;
+    }
 
 
 }
