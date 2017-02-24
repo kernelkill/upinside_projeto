d1ca25a22a49189d427bb1b3d6610b8fece68d8a
Kachorroski <kernelkill@outlook.com>
2017-02-23 21:34:34 -0400

    Criando o arquivo de interface.

--- modulos/03-heranca/class/TrabalhoComInterfaces.class.php
+++ modulos/03-heranca/class/TrabalhoComInterfaces.class.php
@@ -8,5 +8,22 @@
  */
 class TrabalhoComInterfaces
 {
+    public $Aluno;
+    public $Curso;
+    public $Formacao;
+
+    /**
+     * TrabalhoComInterfaces constructor.
+     * @param $Aluno
+     * @param $Curso
+     * @param $Formacao
+     */
+    public function __construct($Aluno, $Curso)
+    {
+        $this->Aluno = $Aluno;
+        $this->Curso = $Curso;
+        $this->Formacao = array();
+    }
+
 
 }
---
+++ modulos/03-heranca/interface/IAluno.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kachorroski
+ * Date: 23/02/17
+ * Time: 21:33
+ */
+
+    interface IAluno{
+
+    }
