4dc25d0f146fa1f9a36cd5f3d5cab065dd89a39c
Kachorroski <kernelkill@outlook.com>
2017-02-28 17:17:01 -0400

    Trabalhando com acesso protegido.

---
+++ modulos/04-encapsulamento/02-acesso-protegido.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kachorroski
+ * Date: 28/02/17
+ * Time: 17:15
+ */
+
+    require ('./inc/Config.inc.php');
+    
+    
+?>
+</body>
+</html>
---
+++ modulos/04-encapsulamento/class/AcessoProtegido.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kachorroski
+ * Date: 28/02/17
+ * Time: 17:14
+ */
+class AcessoProtegido
+{
+
+
+}
