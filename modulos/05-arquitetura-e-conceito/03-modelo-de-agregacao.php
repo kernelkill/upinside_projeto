05c49059b1032c6c55315b013729d9fa752e4261
Kachorroski <kernelkill@outlook.com>
2017-03-02 23:25:38 -0400

    Agregacao, em associação.

--- modulos/05-arquitetura-e-conceito/03-modelo-de-agregacao.php
+++ modulos/05-arquitetura-e-conceito/03-modelo-de-agregacao.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Agregaçao...</title>
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +15,19 @@
  * Date: 02/03/17
  * Time: 23:14
  */
+
+    require ('./inc/Config.inc.php');
+
+    $joabe = new AssociacaoCliente('Joabe Kachorroski','joabe@server.com');
+    
+    $pro = new agregacaoProduto('20','ProPHP',334.90);
+    $wsphp = new agregacaoProduto('21','WS PHP',289.90);
+    $wshtml = new agregacaoProduto('22','WS HTML',289.90);
+
+    $outrocurso = new stdClass();
+    $outrocurso->Produto = '23';
+    $outrocurso->Nome = 'Curso JQuery';
+    $outrocurso->Valor = 400;
+?>
+</body>
+</html>
--- modulos/05-arquitetura-e-conceito/class/agregacaoProduto.class.php
+++ modulos/05-arquitetura-e-conceito/class/agregacaoProduto.class.php
@@ -8,5 +8,45 @@
  */
 class agregacaoProduto
 {
+    private $Produto;
+    private $Nome;
+    private $Valor;
+
+    /**
+     * agregacaoProduto constructor.
+     * @param $Produto
+     * @param $Nome
+     * @param $Valor
+     */
+    public function __construct($Produto, $Nome, $Valor)
+    {
+        $this->Produto = $Produto;
+        $this->Nome = $Nome;
+        $this->Valor = $Valor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProduto()
+    {
+        return $this->Produto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->Nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValor()
+    {
+        return $this->Valor;
+    }
 
 }
