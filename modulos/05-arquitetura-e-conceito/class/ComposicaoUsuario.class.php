8e388d9c69dd805a81db5cc1ea96a6db05f1c8ad
Kachorroski <kernelkill@outlook.com>
2017-03-05 03:08:19 -0400

    Entrando no assunto de compisição.

--- modulos/05-arquitetura-e-conceito/class/ComposicaoUsuario.class.php
+++ modulos/05-arquitetura-e-conceito/class/ComposicaoUsuario.class.php
@@ -8,5 +8,32 @@
  */
 class ComposicaoUsuario
 {
+    private $Nome;
+    private $Email;
+    private $Endereco;
+
+    /**
+     * ComposicaoUsuario constructor.
+     * @param $Nome
+     * @param $Email
+     */
+    public function __construct($Nome, $Email)
+    {
+        $this->Nome = $Nome;
+        $this->Email = $Email;
+    }
+
+    public function CadastrarEndereco($Cidade, $Estado)
+    {
+        $this->Endereco = new ComposicaoEndereco($Cidade, $Estado);
+    }
+
+    /** @return ComposicaoEndereco */
+    public function getEndereco()
+    {
+        return $this->Endereco;
+        
+    }
+
 
 }
