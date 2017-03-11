76f4308de2b88c8b238ddaf45964bfeedfafdaa5
Kachorroski <kernelkill@outlook.com>
2017-03-11 14:47:41 -0400

    Disparando erros em validaçoes.

--- modulos/06-tratamento-de-erros/02-disparando-erros.php
+++ modulos/06-tratamento-de-erros/02-disparando-erros.php
@@ -16,6 +16,20 @@
  * Time: 01:35
  */
 
+    function Erro($Erro, $Mensagem, $Arquivo, $Linha){
+        $erro = ($Erro == E_USER_ERROR ? 'red' : ($Erro == E_USER_WARNING ? 'darkorange' : 'blue'));
+        echo "<p style='color:{$erro}'> Erro na linha # {$Linha}:{$Mensagem}<br>";
+        echo "<small>{$Arquivo}</small></p>";
+
+        if ($Erro == E_USER_ERROR){
+            die("");
+        }
+    }
+
+    set_error_handler('Erro');
+
+
+
     $uso = '12345678908';
     $cpf = '';
     $cpf = '500';
@@ -23,25 +37,20 @@
     $cpf = "abs";
 
 
-    if (!$cpf):
-        trigger_error("Informe o CPF", E_USER_NOTICE);
-    elseif ($cpf == '500'):
-        trigger_error("Formato nao e mais utilizado", E_USER_DEPRECATED);
-    elseif ($cpf == $uso):
-        trigger_error("CPF em uso", E_USER_WARNING);
-    elseif (!preg_match('/ [0-9]*$/i', $cpf) && strlen($cpf) != 11):
-        trigger_error("CPF Invalido", E_USER_ERROR);
-    else:
-        echo "CPF Valido";
-    endif;
-    echo ":)";
+if (!$cpf):
+    trigger_error("Informe o CPF", E_USER_NOTICE);
+elseif ($cpf == '500'):
+    trigger_error("Formato nao e mais utilizado", E_USER_DEPRECATED);
+elseif ($cpf == $uso):
+    trigger_error("CPF em uso", E_USER_WARNING);
+elseif (!preg_match('/ [0-9]*$/i', $cpf) && strlen($cpf) != 11):
+    trigger_error("CPF Invalido", E_USER_ERROR);
+else:
+    echo "CPF Valido";
+endif;
+echo ":)";
 
 
-    function Erro($Erro, $Mensagem, $Arquivo, $Linha){
-        $erro = ($Erro == E_USER_ERROR ? 'red' : ($Erro == E_USER_WARNING ? 'darkorange' : 'blue'));
-        echo "<p style='color:{}'> <br>";
-        echo "<small></small></p>";
-    }
 ?>
 </body>
 </html>
