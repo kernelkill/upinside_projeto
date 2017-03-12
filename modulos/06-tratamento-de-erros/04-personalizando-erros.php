8740a03cb03fb97e176b0f1d7f971d07c770f280
Kachorroski <kernelkill@outlook.com>
2017-03-12 00:46:48 -0400

    Agora iniciamos uma personalização de erros.

--- modulos/06-tratamento-de-erros/04-personalizando-erros.php
+++ modulos/06-tratamento-de-erros/04-personalizando-erros.php
@@ -1,10 +1,11 @@
 <!doctype html>
-<html lang="pt-BR">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/reset.css">
     <title>Personalizando erros</title>
 </head>
 <body>
@@ -16,7 +17,25 @@
  * Time: 15:14
  */
 
+    require ('./_app/Config.inc.php');
 
+    trigger_error('Essa é uma NOTICIA', E_USER_NOTICE);
+    trigger_error('Esse é um ALERTA', E_USER_WARNING);
+    //trigger_error("Esse é um ERRO!", E_USER_ERROR);
+
+    PHPErro(WS_ERROR, "Esse é um ERRO personalizado!", __FILE__, __LINE__);
+    WSErro("Esse é um ACCEPT!", WS_ACCEPT);
+
+try {
+
+    throw new Exception("Essa é uma Exessão!", E_USER_WARNING);
+} catch (Exception $e) {
+
+    PHPErro($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
+    WSErro($e->getMessage(), $e->getCode());
+    WSErro($e->getMessage(), WS_ACCEPT);
+}
+    
 ?>
 </body>
 </html>
--- modulos/06-tratamento-de-erros/_app/Config.inc.php
+++ modulos/06-tratamento-de-erros/_app/Config.inc.php
@@ -5,6 +5,10 @@
  * Date: 01/03/17
  * Time: 00:36
  */
+
+//CONFIGURAÇOES DO SITE. #############
+//AUTO LOAD DE CLASSES. #############
+
 function __autoload($Class) {
 
     $dirName = 'class';
@@ -15,3 +19,38 @@ function __autoload($Class) {
         die("Erro ao incluir {$dirName}/{$Class}.class.php<hr>");
     endif;
 }
+
+//TRATAMENTO DE ERROS. #############
+//CSS constantes :: Mensagens de Erro.
+
+define('WS_ACCEPT','accept');
+define('WS_INFOR','infor');
+define('WS_ALERT','alert');
+define('WS_ERROR','error');
+
+//WSError :: Exibe erros lançados; :: Font
+    function WSErro($ErrMsg, $ErrNo, $ErrDie = null){
+        $CssClass = ($ErrNo == E_USER_NOTICE ? WS_INFOR : ($ErrNo == E_USER_WARNING ? WS_ALERT : ($ErrNo == E_USER_ERROR ? WS_ERROR : $ErrNo)));
+        echo "<p class=\"trigger {$CssClass}\">{$ErrMsg}<span class=\"ajax_close\"></span></p>";
+
+        if ($ErrDie):
+            die;
+        endif;
+
+    }
+
+//PHPErro :: personaliza gatilho do PHP
+    function PHPErro($ErrNo, $ErrMsg, $ErrFile, $ErrLine){
+        $CssClass = ($ErrNo == E_USER_NOTICE ? WS_INFOR : ($ErrNo == E_USER_WARNING ? WS_ALERT : ($ErrNo == E_USER_ERROR ? WS_ERROR : $ErrNo)));
+        echo "<p class=\"trigger {$CssClass}\">";
+        echo "<b>Erro na Linha: #{$ErrLine} ::</b> {$ErrMsg}<br>";
+        echo "<small>{$ErrFile}</small>";
+        echo "<span class=\"ajax_close\"></span></p>";
+
+        if ($ErrNo == E_USER_ERROR):
+            die;
+        endif;
+    }
+
+
+set_error_handler('PHPErro');
